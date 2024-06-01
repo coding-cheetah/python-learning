2b2ee3d8183711859d1ae59b7aeb16f4e48cc138
Ved Bhudhia <ved.bhudhia@gmail.com>
2024-06-01 19:39:37 +0530

    use the binary search algorithm instead of linear searching

--- hackerrank- longest_increasing_sequence.py
+++ hackerrank- longest_increasing_sequence.py
@@ -1,32 +1,54 @@
+def search(arr, num, limits):
+    half_index = (limits[0] + limits[1]) // 2
+    if limits[1] - limits[0] == 0:
+        return half_index if (arr[half_index][1] < num) else half_index + 1
+
+    if limits[1] - limits[0] == 1:
+        if num > arr[half_index][1]:
+            return half_index
+        return half_index + 1 if (arr[half_index + 1][1] < num) else half_index + 2
+
+    if arr[half_index][1] == num:
+        return half_index
+    if arr[half_index][1] < num:
+        return search(arr, num, (limits[0], half_index-1))
+    else:
+        return search(arr, num, (half_index+1, limits[1]))
+
+arr = [(2,12), (1,9)]
+
+# arr = [(5, 15), (4, 14), (3, 11), (2, 10), (1, 4)]
+# print(search(arr,8, (0, len(arr)-1)))
+
+
+
+
+
+
 def longestIncreasingSubsequence(arr):
     info_list = [(1, arr[0])]
     for i in range(1, len(arr)):
+        print(info_list)
         current_value = arr[i]
-        # print ("processing value: ", current_value)
+        print ("processing value: ", current_value)
         seq_count = 1
-        for index, (count_till_prev, prev_value) in enumerate(info_list):
-            # print("\tprocessing the element: ", info_list[index])
-            if current_value > prev_value:
-                #print("here")
-                seq_count = count_till_prev + 1
-                if index == 0:
-                    info_list.insert(index, (seq_count, current_value))
-                    # print("inserting at index: ", index, "the count of ", seq_count)
-                    break
-                else:
-                    info_list[index - 1] = (seq_count, current_value)
-                    # print("updating at index: ", index, "the count of ", seq_count)
-                    break
-        if seq_count == 1:
-            info_list[-1] = (seq_count, current_value)
-            # print("updating the last element to the count of ", seq_count)
-            # print("no. is bigger")
+        index = search(info_list, current_value, (0, len(info_list)-1))
+        if index == len(info_list):
+            info_list[- 1] = (seq_count, current_value)
+            continue
+        print("index = ",index)
+        seq_count = info_list[index][0] + 1
+        if index == 0:
+            info_list.insert(index, (seq_count, current_value))
+            print("inserting at index: ", index, "the count of ", seq_count)
+        else:
+            info_list[index - 1] = (seq_count, current_value)
+            print("updating at index: ", index, "the count of ", seq_count)
 
-        # print(info_list)
+
 
     return info_list[0][0]
 
 arr = [9, 7, 5, 4, 18, 19, 10, 11, 14, 15]
 # arr = [2, 7, 4, 3, 8]
-
 print(longestIncreasingSubsequence(arr))
