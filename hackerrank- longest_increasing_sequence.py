ac8ef5fb179d431be78c0a84a4c0644262bc11f8
Ved Bhudhia <vedbhudhia@gmail.com>
2024-06-01 12:02:53 +0530

    Change the data stored in the info list
    Reduce the info list size by just keeping the minimum value per sequence count.
    We can further improve the runtime of the algorithm by implementing a binary serach instead of the existing linear search

--- hackerrank- longest_increasing_sequence.py
+++ hackerrank- longest_increasing_sequence.py
@@ -2,19 +2,27 @@ def longestIncreasingSubsequence(arr):
     info_list = [(1, arr[0])]
     for i in range(1, len(arr)):
         current_value = arr[i]
-        print ("processing value: ", current_value)
+        # print ("processing value: ", current_value)
         seq_count = 1
         for index, (count_till_prev, prev_value) in enumerate(info_list):
-            print("\tprocessing the element: ", info_list[index])
+            # print("\tprocessing the element: ", info_list[index])
             if current_value > prev_value:
                 #print("here")
-                seq_count = count_till_prev + 1 
-                break
+                seq_count = count_till_prev + 1
+                if index == 0:
+                    info_list.insert(index, (seq_count, current_value))
+                    # print("inserting at index: ", index, "the count of ", seq_count)
+                    break
+                else:
+                    info_list[index - 1] = (seq_count, current_value)
+                    # print("updating at index: ", index, "the count of ", seq_count)
+                    break
+        if seq_count == 1:
+            info_list[-1] = (seq_count, current_value)
+            # print("updating the last element to the count of ", seq_count)
             # print("no. is bigger")
-        print("inserting at index: ", index, "the count of ", seq_count)
-        info_list.insert(index, ((seq_count, current_value)))
 
-        print(info_list)
+        # print(info_list)
 
     return info_list[0][0]
 
